b58bb2af5adcb863bcddb2c504c099e00ba68899
unnamed-horse <nico.water@pm.me>
2025-11-12 04:12:29 +0000

    none-ls added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,73 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		'nvimtools/none-ls.nvim',
+		dependencies = {
+			'nvimtools/none-ls-extras.nvim',
+			'jay-babu/mason-null-ls.nvim',
+
+		},
+		config = function()
+			require('mason-null-ls').setup {
+				ensure_installed = {
+					'ruff',
+					'prettier',
+					'shfmt',
+				},
+				automatic_installation = true,
+			}
+
+			local null_ls = require 'null-ls'
+			local sources = {
+				require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
+				require 'none-ls.formatting.ruff_format',
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown' } },
+				null_ls.builtins.formatting.shfmt.with { args = { '-i', '4' } },
+			}
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end
+	}
+}
+
+-- return {
+-- 	"nvimtools/none-ls.nvim",
+-- 	dependencies = {
+-- 		"nvimtools/none-ls-extras.nvim",
+-- 	},
+-- 	config = function()
+-- 		local null_ls = require("null-ls")
+-- 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+-- 		null_ls.setup({
+-- 			sources = {
+-- 				null_ls.builtins.formatting.stylua,
+-- 				null_ls.builtins.formatting.ruff,
+-- 				require("none-ls.diagnostics.ruff"),
+-- 				require("none-ls.diagnostics.eslint"),
+-- 				null_ls.builtins.formatting.prettier.with({
+-- 					filetypes = { "json", "markdown", "yaml" },
+-- 				}),
+-- 			},
+-- 			on_attach = function(client, bufnr)
+-- 				if client.supports_method("textDocument/formatting") then
+-- 					vim.api.nvim_clear_autocmds({
+-- 						group = augroup,
+-- 						buffer = bufnr,
+-- 					})
+-- 					vim.api.nvim_create_autocmd("BufWritePre", {
+-- 						group = augroup,
+-- 						buffer = bufnr,
+-- 						callback = function()
+-- 							vim.lsp.buf.format({ bufnr = bufnr })
+-- 						end,
+-- 					})
+-- 				end
+-- 			end,
+-- 		})
+-- 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+-- 	end,
+-- }
